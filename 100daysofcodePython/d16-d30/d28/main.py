c5d0778fe60f4ab35aa4fd82e874b18bee36e246
rohit <rohitracer0023@gmail.com>
2026-02-02 02:05:09 +0530

    d29 - pwd manager tkinter

--- 100daysofcodePython/d16-d30/d28/main.py
+++ 100daysofcodePython/d16-d30/d28/main.py
@@ -69,9 +69,10 @@ window.configure(padx=100, pady=50)  # add padding around the window
 canvas = Canvas(width=200, height = 224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file=image_path)
 canvas.create_image(100, 112, image=tomato_img)
+canvas.grid(column=1, row=1)
+
 
 timer_text = canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(column=1, row=1)
 
 
 timer_label = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
---
+++ 100daysofcodePython/d16-d30/d29/main.py
@@ -0,0 +1,95 @@
+from tkinter import *
+from tkinter import messagebox
+import pyperclip
+
+image_path = '100daysofcodePython/d16-d30/d29/logo.png'
+file_path = '100daysofcodePython/d16-d30/d29/file.txt'
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random
+
+def generate_pwd():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = [random.choice(letters) for _ in range(nr_letters)]
+    password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+    password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+
+    random.shuffle(password_list)
+
+    password = ''.join(password_list)
+    password_entry.delete(first=0, last=END)
+    password_entry.insert(0,string=password)
+    pyperclip.copy(password_entry.get())
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_pwd():
+    website_detail = website_entry.get()
+    email_detail = email_entry.get()
+    password_detail = password_entry.get()
+    
+    if len(website_detail)==0 or len(password_detail)==0:
+        messagebox.showinfo(title='Info', message='Please don\'t leave any fields empty!!')
+        
+    else:
+        is_ok = messagebox.askokcancel(title=website_detail, message=f'Details: \nEmail:{email_detail} \nPassword: {password_detail}')
+
+        if is_ok:
+            with open(file=file_path, mode='a') as f:
+                text = f'{website_detail} | {email_detail} | {password_detail} \n'
+                f.write(text)
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title('Password Manager')
+window.configure(bg='white', padx=60, pady=60)
+window.tk_setPalette(background='white')
+
+
+canvas = Canvas(width=200, height=200)
+logo_image = PhotoImage(file=image_path)
+
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text='Website: ')
+website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1,column=1, columnspan=2)
+website_entry.focus()
+
+
+email_label = Label(text='Email/Username: ')
+email_label.grid(row=2, column=0)
+
+email_entry = Entry(width=35)
+email_entry.insert(0, 'rohitracer0023@gmail.com')
+email_entry.grid(row=2,column=1, columnspan=2)
+
+
+password_label = Label(text='Password: ')
+password_label.grid(row=3, column=0)
+
+password_entry = Entry(width=19)
+password_entry.grid(row=3,column=1)
+
+
+generate_pwd_button = Button(text='Generate Password', width=11, command=generate_pwd)
+generate_pwd_button.grid(row=3, column=2)
+
+
+add_button = Button(text='Add', width=33, command=save_pwd)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
