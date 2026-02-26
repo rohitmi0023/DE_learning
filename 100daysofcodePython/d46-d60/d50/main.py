0483bffe2fce45809eda0ed5f049c6f2d6912d75
rohit <rohitracer0023@gmail.com>
2026-02-27 00:47:06 +0530

    d50 to d53

---
+++ 100daysofcodePython/d46-d60/d50/main.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+FB_EMAIL = 'FB_MAIL'
+FB_PASSWORD = 'FB_PASSWORD'
+
+driver = webdriver.Chrome()
+
+driver.get("http://www.tinder.com")
+
+sleep(2)
+login_button = driver.find_element(By.XPATH, value='//*[text()="Log in"]')
+login_button.click()
+
+sleep(2)
+fb_login = driver.find_element(By.XPATH, value='//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login.click()
+
+
+#Switch to Facebook login window
+sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
+
+#Login and hit enter
+email = driver.find_element(By.XPATH, value='//*[@id="email"]')
+password = driver.find_element(By.XPATH, value='//*[@id="pass"]')
+email.send_keys(FB_EMAIL)
+password.send_keys(FB_PASSWORD)
+password.send_keys(Keys.ENTER)
+
+#Switch back to Tinder window
+driver.switch_to.window(base_window)
+print(driver.title)
+
+#Delay by 5 seconds to allow page to load.
+sleep(5)
+
+#Allow location
+allow_location_button = driver.find_element(By.XPATH, value='//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+allow_location_button.click()
+
+#Disallow notifications
+notifications_button = driver.find_element(By.XPATH, value='//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+notifications_button.click()
+
+#Allow cookies
+cookies = driver.find_element(By.XPATH, value='//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+cookies.click()
+
